89c30d32573b734ae1c43ec0ec36b8d9bff61f70
Konstantin <alx0000@yandex.ru>
2015-03-30 12:00:42 +0300

    Init Pwd

---
+++ Model/Container.cs
@@ -0,0 +1,10 @@
+﻿
+namespace PwdGen.Model
+{
+    class Container
+    {
+        public string Id { get; set; }
+        public string Pass { get; set; }
+        public string Date { get; set; }
+    }
+}
---
+++ ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+using PwdGen.Infrastructure;
+using PwdGen.Model;
+
+namespace PwdGen.ViewModel
+{
+    class MainWindowViewModel : ViewModelBase
+    {
+        public ICommand StartCommand { get; set; }
+        public ICommand StartCommand1 { get; set; }
+        private readonly HelperGenerator _helper = new HelperGenerator();
+        public MainWindowViewModel()
+        {
+            // TODO: Необходимо разобрать логику кнопки старта у вована слишком много не описуемого говнокода!
+            
+            StartCommand = new RelayCommand(obj => MessageBox.Show("Логика старта тест -> " 
+                + Environment.NewLine + _helper.GetPass() 
+                + Environment.NewLine + _helper.GetData()
+                + Environment.NewLine + Id3()));
+
+            StartCommand1 = new RelayCommand(obj => MessageBox.Show("Логика старта тест -> "
+                + Environment.NewLine + _helper.GetPass()
+                + Environment.NewLine + _helper.GetData()
+                + Environment.NewLine + Id1()));
+
+        }
+
+         string Id3()
+         {
+             return _helper.GetId();
+         }
+
+         string Id1()
+         {
+             return _helper.GetId();
+         }
+
+    }
+}
