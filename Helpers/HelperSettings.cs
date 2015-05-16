584c797d648d2c78bc9ef67ca6dd93231ee9f32c
Konstantin <alx0000@yandex.ru>
2015-05-16 23:03:03 +0300

    Рефактор Настроек

--- Helpers/HelperSettings.cs
+++ Helpers/HelperSettings.cs
@@ -25,7 +25,6 @@ namespace PwdGen.Helpers
                 WriteState(value, "InitUsb");
             }
         }
-
         public static State PrintState
         {
             get
@@ -38,14 +37,12 @@ namespace PwdGen.Helpers
                 WriteState(value, "Printer");
             }
         }
-
         private static State ReadState(string nameParam)
         {
             var parser = new FileIniDataParser();
             IniData data = parser.ReadFile(PathConfigurationFile, Encoding.UTF8);
             return data["Options"][nameParam] == "true" ? State.On : State.Off;
         }
-
         private static void WriteState(State val, string nameParam)
         {
             var parser = new FileIniDataParser();
