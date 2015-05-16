56c42e36de6771242df48c286c82bce07f069fe1
Konstantin <alx0000@yandex.ru>
2015-05-16 23:01:23 +0300

    Добавил интерфейсы

---
+++ Infrastructure/IFileManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+using System.Xml.Linq;
+using PwdGen.Model;
+
+namespace PwdGen.Infrastructure
+{
+    interface IFileManager
+    {
+        void Save(Container container, string path);
+        Container Load(string path);
+        bool IsExist(string path);
+        void SaveHistory(Container container, string path);
+    }
+}
